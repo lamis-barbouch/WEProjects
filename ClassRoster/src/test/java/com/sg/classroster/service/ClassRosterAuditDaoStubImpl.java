824e9e5a1525fd6cd0a935e0a9db36daf5853c3e
lamis <lamis.barbouch@esprit.tn>
2022-12-20 14:53:37 -0500

    add Class Roster project

---
+++ ClassRoster/src/test/java/com/sg/classroster/service/ClassRosterAuditDaoStubImpl.java
@@ -0,0 +1,16 @@
+package com.sg.classroster.service;
+
+import com.sg.classroster.dao.ClassRosterAuditDao;
+import com.sg.classroster.dao.ClassRosterPersistenceException;
+import com.sg.classroster.dto.Student;
+
+public class ClassRosterAuditDaoStubImpl implements ClassRosterAuditDao {
+
+    @Override
+    public void writeAuditEntry(String entry) throws ClassRosterPersistenceException {
+        //do nothing . . .
+    }
+
+    public Student onlyStudent;
+
+}
